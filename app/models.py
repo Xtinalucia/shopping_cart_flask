cfea596f17a4a52611aa291e8f50ed72914f2bbb
xtinalucia <christina.lucia123@gmail.com>
2021-10-19 00:40:35 -0500

    night commit

---
+++ app/models.py
@@ -0,0 +1,52 @@
+from app import db, login
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+@login.user_loader
+def get_user(user_id):
+    return User.query.get(user_id)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    products = db.relationship('Product', secondary='cart')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = generate_password_hash(password)
+
+    def __repr__(self):
+        return f'<User|{self.username}>'
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Numeric(5,2), nullable=False)
+    image_url = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    users = db.relationship('User', secondary='cart')
+
+
+    def __init__(self, name, price, image_url=None):
+        self.name = name
+        self.price = price
+        self.image_url = image_url
+
+    def __repr__(self):
+        return f'<Product | {self.name}>'
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+         
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,79 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, redirect, url_for
+from flask_login import login_user, logout_user, login_required, current_user
+from app.forms import LoginForm, RegistrationForm
+from app.models import Product, User
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', name='you')
+    return render_template('index.html', name='Brian')
 
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    register_form = RegistrationForm()
+    if register_form.validate_on_submit():
+        username = register_form.username.data
+        password = register_form.password.data
+        new_user = User(username, password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('register.html', form=register_form)
+
+
+@app.route('/products')
+def products():
+    all_products = Product.query.all()
+    return render_template('products.html', products=all_products)
+
+
+@app.route('/products/<prod_id>')
+def product_detail(prod_id):
+    print(prod_id)
+    product = Product.query.get_or_404(prod_id)
+    return render_template('product_detail.html', product=product)
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if user is None or not user.check_password(password):
+            print('The user username or password was incorrect')
+            return redirect(url_for('login'))
+
+        login_user(user)
+        print('The user has been logged in.')
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/my-cart')
+@login_required
+def my_cart():
+    my_products = current_user.products
+    return render_template('products.html', products=my_products)
+
+
+
+@app.route('/add-to-cart/<prod_id>')
+@login_required
+def add_to_cart(prod_id):
+    product = Product.query.get_or_404(prod_id)
+    current_user.products.append(product)
+    db.session.commit()
+    return redirect(url_for('my_cart'))
